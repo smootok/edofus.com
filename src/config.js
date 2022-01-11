95396cd4b8b8e54aabf3710467ba97044989557b
Yassine Amine <smootok@gmail.com>
2022-01-12 00:31:32 +0100

    update api base url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-exports.apiBaseUrl = 'https://api.edofus.com/api/v1'
+exports.apiBaseUrl = 'https://edofus.smootok.com/api/v1'
 
-exports.imagesBaseUrl = 'https://api.edofus.com/images'
+exports.imagesBaseUrl = 'https://edofus.smootok.com/images'
